fe07c1bc8393539e9949bedb8e8de90ee467fab3
Albert <albertmoczadlo@gmail.com>
2022-12-01 09:36:36 +0100

    Added new project MVC

---
+++ HouseBills/HouseBills.WebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HouseBills.WebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
